f82ebca1c7ea1f309906bf7ebbcc0e53f8d71723
shreyaskad25-pixel <shreyask.ad25@bmsce.ac.in>
2026-09-24 11:47:31 +0530

    Enhance home page with ambient effects and scroll depth animation

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+  constructor(private elementRef: ElementRef<HTMLElement>) {}
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    const scrollDepth = window.scrollY;
+    this.elementRef.nativeElement.style.setProperty('--scroll-depth', `${scrollDepth}px`);
+  }
+}
